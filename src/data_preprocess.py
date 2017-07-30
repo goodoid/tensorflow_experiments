05443f79cd0723854a85cfb623660540ca60db1d
wangguo <wangguo@didichuxing.com>
2017-07-30 17:08:57 +0800

    extract max value in 120 sensors

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -3,6 +3,7 @@ import os
 import tensorflow as tf
 import numpy
 input_data_path="./data/16-rawdata-max/huajiaoyou-ascii/2016_03_25/merged/"
+max_data_file="./data/16-rawdata-max/max.txt"
 cat_to_int={'H':[0,0,1],'Q':[0,1,0],'T':[1,0,0]}
 #cat_to_int={'H':1,'Q':2,'T':3}
 #train_data_file={'H':''}
@@ -10,6 +11,42 @@ cat_to_int={'H':[0,0,1],'Q':[0,1,0],'T':[1,0,0]}
 output_path="./output/"
 save_file = output_path+'/bincounter.ckpt'
 sess = tf.InteractiveSession()
+def max_extract(max_file):
+    maxf = open(max_file)
+    train_x=[]
+    train_y=[]
+    test_x=[]
+    test_y=[]
+    group_to_data = dict()
+    line = maxf.readline().strip()
+    columns = line.split("\t")
+    print line
+    group_idx = columns.index("Groups") + 1
+    line = maxf.readline().strip()
+    while line:
+        columns = line.split("\t")
+        x = columns[1:group_idx]
+        y = columns[group_idx][0]
+        if not group_to_data.has_key(y):
+            group_to_data[y] = [x]
+        else:
+            group_to_data[y].append(x)
+        print "len of ",columns[group_idx],y,len(group_to_data[y])
+        line = maxf.readline().strip()
+    print "len of group:",len(group_to_data)
+    for k,v in group_to_data.items():
+        vj = 0
+        while vj <len(v)-1:
+            train_x.append(v[vj])
+            train_y.append(cat_to_int[k])
+            vj+=1
+        test_x+=v[-1]
+        test_y+=cat_to_int[k]
+    print "train:",len(train_x),len(train_y)
+    print "test:",len(test_x),len(test_y)
+    print train_x,train_y
+    return [train_x, train_y],[test_x,test_y]
+
 def normalize(data_path):
     files = os.listdir(data_path)
     #print len(files),files
@@ -68,6 +105,11 @@ def normalize(data_path):
                 #sys.exit()
                 train_x.append(new_s.tolist()[0])
                 train_y.append(cat_to_int[cat])
+    xof = open('train_x','w')
+    for tx in train_x:
+        xof.write("\t".join(map(lambda x:str(x),tx))+"\n")
+    xof.close()
+    sys.exit()
     print len(train_x),len(train_x[0])#,train_x
     print len(train_y)#,train_y #train_x  = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], #              [1, 1, 0], [1, 1, 1]]
     #train_y = [[0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 0],
@@ -125,17 +167,16 @@ def train(data,neuron_struct):
     with tf.name_scope('train'):
         train_step = tf.train.RMSPropOptimizer(0.25, momentum=0.5).minimize(cross_entropy)
     with tf.name_scope('optimizer'):
-        optimizer = tf.train.GradientDescentOptimizer(0.1)
+        optimizer = tf.train.GradientDescentOptimizer(1.0)
         train_ops = optimizer.minimize(cross_entropy)
-    #sess.run(tf.global_variables_initializer())
-    #sess.run(tf.global_variables_initializer())
-    sess.run(tf.initialize_all_variables())
+    sess.run(tf.global_variables_initializer())
+    #sess.run(tf.initialize_all_variables())
     is_converge = False
     for i in range(10001):
       if i%1000 == 0:
         train_error = cross_entropy.eval(feed_dict={x: train_x, y_:train_y})
         print("step %d, training error  %g"%(i, train_error))
-        if abs(train_error) < 0.0005:
+        if abs(train_error) < 0.5:
             is_converge = True
             break
       
@@ -161,24 +202,25 @@ if __name__ == "__main__":
        {
            "name":"layer1",
            #"struct":[18,8,"tf.nn.sigmod"],
-           "struct":[18,20,"tf.nn.relu"],
-       },
-       {
-           "name":"layer2",
-           "struct":[20,18,"tf.nn.sigmoid"],
-       },
-       {
-           "name":"layer3",
            "struct":[18,15,"tf.nn.relu"],
        },
+        {
+           "name":"layer3",
+           "struct":[15,13,"tf.nn.relu"],
+       },
+{
+           "name":"layer3",
+           "struct":[13,9,"tf.nn.relu"],
+       },
+
        {
            "name":"layer3",
-           "struct":[15,10,"tf.nn.relu"],
+           "struct":[9,17,"tf.nn.relu"],
        },
 
        {
            "name":"layer4",
-           "struct":[10,5,"tf.nn.sigmoid"],
+           "struct":[17,5,"tf.nn.sigmoid"],
        },
        {
            "name":"layer5",
@@ -186,7 +228,8 @@ if __name__ == "__main__":
        },
    ])
  
-    train_data,test_data = normalize(input_data_path)
+    #train_data,test_data = normalize(input_data_path)
+    train_data,test_data = max_extract(max_data_file)
     #if os.path.isfile(save_file+".meta"):
     #    test(test_data)
     #else:
